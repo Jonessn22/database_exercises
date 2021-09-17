1b6fa4605c7937a3ebe274b4550df35da7031722
Stephanie Jones <stephanie.nicole.jones78@gmail.com>
2021-09-17 14:07:51 -0500

    commit exercises for SQL Tables section

---
+++ table_exercises.sql
@@ -0,0 +1,14 @@
+SHOW TABLES;
+
+DESCRIBE employees;
+
+/*
+six) Numer/ic typ/e: emp_no
+--seven) Strin/g typ/e: first_name, last_name, gender (could also be boo/l typ/e)
+--eight) Dat/e typ/e: birth_date, hire_date
+--nine) they both have fields with data types varchar
+*/
+
+DESCRIBE departments;
+
+SHOW CREATE DATABASE dept_manager;
