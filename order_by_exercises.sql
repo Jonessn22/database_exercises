f2dfd7e2c492c4071b457e360cc778ca8dac0f98
Stephanie Jones <stephanie.nicole.jones78@gmail.com>
2021-09-20 15:28:10 -0500

    through no 5

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -44,3 +44,44 @@ ORDER BY first_name DESC, last_name DESC;
 
 # The first and last name of the last person in the table is Vidya Zweizig.
 
+/* 4. Find all employees with first names 'Irena', 'Vidya', or 'Maya', and order your results returned by last name and then first name. In your comments, answer: What was the first and last name in the first row of the results? What was the first and last name of the last person in the table? */
+
+SELECT first_name, last_name
+FROM employees
+WHERE first_name = 'Irena' 
+	OR first_name = 'Vidya'
+	OR first_name = 'Maya'
+ORDER BY last_name, first_name;
+
+# The first and last name in the first row of the results is Irena Acton.
+
+SELECT first_name, last_name
+FROM employees
+WHERE first_name = 'Irena' 
+	OR first_name = 'Vidya'
+	OR first_name = 'Maya'
+ORDER BY last_name DESC, first_name DESC;
+
+# The first and last name of the last person in the table is Maya Zyda.
+
+/* 5. Write a query to to find all employees whose last name starts and ends with 'E'. Sort the results by their employee number. Enter a comment with the number of employees returned, the first employee number and their first and last name, and the last employee number with their first and last name. */
+
+EXPLAIN employees;
+SELECT first_name, last_name, emp_no
+FROM employees
+WHERE last_name LIKE 'E%'
+	AND last_name LIKE '%E'
+ORDER BY emp_no;
+
+# There are 899 employees whose last name starts and ends with 'E'.
+# The first employee number is 10021, with corresponding first and last name of Ramzi Erde.
+
+SELECT first_name, last_name, emp_no
+FROM employees
+WHERE last_name LIKE 'E%'
+	AND last_name LIKE '%E'
+ORDER BY emp_no desc;
+
+# The last employee number is 499648, with corresponding first and last name of Tadahiro Erde.
+
+
