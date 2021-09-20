3ad449c9d65f9edf8a12c099568218d1d4c3c4d9
Stephanie Jones <stephanie.nicole.jones78@gmail.com>
2021-09-20 14:33:32 -0500

    initial commit

---
+++ order_by_exercises.sql
@@ -0,0 +1,6 @@
+USE employees;
+EXPLAIN employees;
+
+/* 1. Create a new file named order_by_exercises.sql and copy in the contents of your exercise from the previous lesson. */
+# see end of file for contents of previous exercise
+
