f7d16f2250e3509db380c4ecee6d52d06571e5d6
Stephanie Jones <stephanie.nicole.jones78@gmail.com>
2021-09-21 08:28:47 -0500

    through no 2

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,3 +1,19 @@
 #LIMIT EXERCISES
 
 /* 1. Create a new SQL script named limit_exercises.sql. */
+
+/* 2. List the first 10 distinct last name sorted in descending order. */
+SELECT DISTINCT last_name
+FROM employees
+LIMIT 10;
+
+/* Facello
+Simmel
+Bamford
+Koblick
+Maliniak
+Preusig
+Zielinski
+Kalloufi
+Peac
+Piveteau */
