461488ebe58a08d45f8f41f45ae709537b6347f3
Stephanie Jones <stephanie.nicole.jones78@gmail.com>
2021-09-27 13:28:36 -0500

    through 6

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -5,9 +5,9 @@ use employees;
 select emp_no as 'Employee Number', concat(first_name, ' ', last_name) as 'Employee Name', hire_date as 'Hire Date', to_date
 from employees
 join dept_emp_latest_date
--- where to_date like '9999%'
 using(emp_no)
-where hire_date =
+where to_date like '9999%'
+and hire_date =
 	(
 	select hire_date
 	from employees
@@ -20,8 +20,8 @@ select title as 'Title'
 from titles
 join employees
 using(emp_no)
--- where to_date like '9999%'
-where emp_no in
+where to_date like '9999%'
+and emp_no in
 	(
 	select emp_no
 	from employees
@@ -30,14 +30,14 @@ where emp_no in
 group by title
 order by title;
 
--- 3. How many people in the employees table are no longer working for the company? Give the answer in a comment in your code.
-select count(*)
+-- 3. How many people in the employees table are no longer working for the company? Give the answer in a comment in your code. 59,900
+select count(*) 
 from employees
 join dept_emp_latest_date
 using(emp_no)
 where to_date < curdate(); 
 
-select count(o)
+select count(*)
 from (
 	select *
 	from employees
@@ -47,6 +47,45 @@ from (
 	) 
 as o;
 
+-- 4. Find all the current department managers that are female. List their names in a comment in your code.
+select concat(first_name, ' ', last_name) as Name
+from employees
+where emp_no in 
+		(
+		select emp_no
+		from dept_manager
+		where to_date > curdate()
+		)
+and gender = 'F'
+order by first_name;
+		
+-- 5. Find all the employees who currently have a higher salary than the companies overall, historical average salary.
+select emp_no as 'Employee No', salary as 'Salary', concat(first_name, ' ', last_name) as 'Name' 
+from salaries
+join employees
+using(emp_no)
+where salary > 
+	(
+	select avg(salary) 
+	from salaries
+	)
+and to_date > curdate()
+order by salary;
+
+-- 6. How many current salaries are within 1 standard deviation of the current highest salary? (Hint: you can use a built in function to calculate the standard deviation.) What percentage of all salaries is this?
+select salary 
+from salaries
+where salary > 
+	(
+	select salary
+	where 
+	from salaries
+	);
+
+
+
+	select stddev(salary)
+	from salaries;
 
 
 
